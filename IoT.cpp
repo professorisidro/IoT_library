f83d80ba24c7659f5531d3fe6865e46cf4e5634d
Tiago Queiroz <contato@tiago.eti.br>
2016-10-30 12:02:30 -0200

    Arrumando identação

--- IoT.cpp
+++ IoT.cpp
@@ -3,56 +3,59 @@
 #include "IoT.h"
 
 
-void    defineRele(int pin){
-   pinMode(pin, OUTPUT);
+void defineRele(int pin){
+  pinMode(pin, OUTPUT);
 }
-void    ligaRele(int pin){
-   digitalWrite(pin, HIGH);
+
+void ligaRele(int pin){
+  digitalWrite(pin, HIGH);
 }
-void    desligaRele(int pin){
-   digitalWrite(pin,LOW);
+
+void desligaRele(int pin){
+  digitalWrite(pin,LOW);
 }
+
 void defineLed(int led){
-    pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
 void acendeLed(int pin){
-	digitalWrite(pin, HIGH);
+  digitalWrite(pin, HIGH);
 }
 
 void apagaLed(int pin){
-	digitalWrite(pin, LOW);
+  digitalWrite(pin, LOW);
 }
 
 void brilhaLed(int pin, int brilho){
-	analogWrite(pin, brilho);
+  analogWrite(pin, brilho);
 }
 
 void espera(long time){
-	delay(time);
+  delay(time);
 }
 
 void botao(int pin){
-	pinMode(pin, INPUT_PULLUP);
+  pinMode(pin, INPUT_PULLUP);
 }
 
 uint8_t verificaBotao(int pin){
-	return digitalRead(pin);
+  return digitalRead(pin);
 }
 
 int botaoLigado(int pin){
-	return (digitalRead(pin) == LOW);
+  return (digitalRead(pin) == LOW);
 }
 
 int botaoDesligado(int pin){
-	return (digitalRead(pin) == HIGH);
+  return (digitalRead(pin) == HIGH);
 }
+
+
 uint8_t Motor::conectar(int pin){
-    Servo::attach(pin);
+  Servo::attach(pin);
 }
+
 void Motor::girar(int graus){
-    Servo::write(graus);
+  Servo::write(graus);
 }
-
-
-
