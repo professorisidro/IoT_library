e715a5283983c05c9da435e194af8d0ddbc14010
Professor Isidro <fmassetto@gmail.com>
2016-11-21 16:49:53 -0200

    funcoes e operacoes logicas

--- IoT.h
+++ IoT.h
@@ -4,33 +4,50 @@
 #include "Arduino.h"
 #include "Servo.h"
 
+
+// algumas constantes
 #define ENTRADA   INPUT
 #define SAIDA     OUTPUT
 #define LIGADO    0
 #define DESLIGADO 1
-#define se        if
-#define senao     else
-#define repita(x) for (int _Iot_counter=0; _Iot_counter < x ; _Iot_counter++)
-#define Numero    int
 
 
+// redefinicoes sintaticas
+#define se             if
+#define senao          else
+#define repita(x)      for (int _Iot_counter=0; _Iot_counter < x ; _Iot_counter++)
+#define Numero         int
+#define NumeroDecimal  float
+
+// operacoes Logicas
+#define  E   &&
+#define  OU  ||
+#define  NAO  !
+
+// funcoes de LED
+void    defineLed(int pin);
 void    acendeLed(int pin);
 void    apagaLed(int pin);
+void    brilhaLed(int pin, int brilho);
 void    espera(long mili);
 
+// funcoes do sensor HC-SR04
 void    defineSensor(int trig, int echo);
 int     calculaDistanciaSensor(int trig, int echo);
 
+// funcoes de Rele
 void    defineRele(int pin);
 void    ligaRele(int pin);
 void    desligaRele(int pin);
-void    defineLed(int pin);
-void    brilhaLed(int pin, int brilho);
+
+// funcoes de Botao (pushbutton)
 void    botao(int pin);
 uint8_t verificaBotao(int pin);
 int     botaoLigado(int pin);
 int     botaoDesligado(int pin);
 
+
+// funcoes de Servomotor
 class Motor : Servo{
 public:
   uint8_t conectar(int pin);
